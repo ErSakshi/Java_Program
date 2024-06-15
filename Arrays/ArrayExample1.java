8a2ea5ec980e521ad76395c281091d509880fba1
ErSakshi <sakshirajeardad@gmail.com>
2024-06-15 12:56:23 +0530

    Array folder del

--- Arrays/ArrayExample1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class ArrayExample1
-{
-	public static void main(String[] args)
-	 {
-		char a[][]={{'a','b'},{'c','d','e'},{'f','g'}};
-		System.out.print(Arrays.deepToString(a));
-	}
-}
--- Arrays/ArrayExample2.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class ArrayExample2
-{
-	public static void main(String[] args) 
-	{
-	   byte a[][][]={{{10,20},{30,40,50}},{{60},{70,80,90,100}}};
-	   System.out.println(Arrays.deepToString(a));	
-	}
-}

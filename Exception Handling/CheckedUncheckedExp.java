440432e7d5b7b9953441de9018f182cfcb51ed74
ErSakshi <sakshirajeardad@gmail.com>
2024-06-18 16:57:22 +0530

    Exception Handling  18-June-2024

---
+++ Exception Handling/CheckedUncheckedExp.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class CheckedUncheckedExp
+{
+	public static void main(String[] args) 
+	 {
+	//   int[]arr={1,2,3,4};
+	//   for(int i=0;i<=arr.length;i++)  //ArrayIndexOutOfBounds
+	//   {
+	//   	System.out.println(arr[i]);  
+	//   }	
+
+		// String a=null;
+		// if(a.isEmpty())
+		// {
+		// 	System.out.println("Hii");   //Null-Pointer Exception
+		// }
+
+	   Scanner sc=new Scanner(System.in);
+	   int num=sc.nextInt();  // if we enter the String value then it is an InputMismatchedException
+	}
+}
